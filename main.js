5c03d6bdf8305d37aebb818980ba4b5f9f0060a4
Gabriel <gabriel.dario.arley.hallden@skola.botkyrka.se>
2024-11-05 15:55:38 +0100

    Jag har lagt till flera html dokument, där man kan ändra till för att nå olika områden man vill göra övningar till.

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
-//Dessa funktioner är väldigt enkla
-function rättaSvar1() {
-    let svar = document.getElementById("svar1").value;
-    let rättSvar = 8; //Det är bara att ändra detta för att ändra svar och lägga bilder
+//funktion för att rätta svar
+function rättaSvar(frågaId, feedbackId, rättSvar, knappId) {
+    let svar = document.getElementById(frågaId).value;
     if (svar == rättSvar) {
-        document.getElementById("feedback1").innerHTML = "Rätt svar!";
+        document.getElementById(feedbackId).innerHTML = "Rätt svar! +1 poäng";
     } else {
-        document.getElementById("feedback1").innerHTML = "Fel svar. Försök igen.";
+        document.getElementById(feedbackId).innerHTML = "Fel svar. Försök igen.";
     }
-}
-
-function rättaSvar2() {
-    let svar = document.getElementById("svar2").value;
-    let rättSvar = 6; 
-    if (svar == rättSvar) {
-        document.getElementById("feedback2").innerHTML = "Rätt svar!";
-    } else {
-        document.getElementById("feedback2").innerHTML = "Fel svar. Försök igen.";
-    }
+    
+    
+    document.getElementById(knappId).disabled = true;
 }
