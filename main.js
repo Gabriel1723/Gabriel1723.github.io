5346f7a9b22f2de5c585d35c4f222e787cf38a6d
Gabriel <gabriel.dario.arley.hallden@skola.botkyrka.se>
2024-12-10 15:56:43 +0100

    Har skapat en enkel timer funktion i JavaScript, som fungerar som det ska. Det fortfarande behövs att lagra poäng, genom till exempel localStorage.

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
+
 //funktion för att rätta svar
 function rättaSvar(frågaId, feedbackId, rättSvar, knappId) {
     let svar = document.getElementById(frågaId).value;
-    if (svar == rättSvar) {
+    if (svar == rättSvar && knappFlagga == false) {
         document.getElementById(feedbackId).innerHTML = "Rätt svar! +1 poäng";
     } else {
         document.getElementById(feedbackId).innerHTML = "Fel svar.";
-    }
+    };
     
     
     document.getElementById(knappId).disabled = true;
 }
+function timerFunktion() {
+    alert('Nu är du blockerat från att svara');
+        let knappar = document.querySelectorAll("button");
+        knappar.forEach(knapp => {
+            knapp.disabled = true;
+  })
+  };
+
+
+
+  //måste använda windows.localstorage för att lagra poäng bland annat
